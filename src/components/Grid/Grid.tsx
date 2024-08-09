361ba546c80902021ca4939b8dd5101ce3cbafb8
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-08-09 09:10:43 -0300

    Primeiro commit

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Grid() {
+  return (
+    <div className='Grid grid-cols-12'>
+    <div className='col-spam-1 bg-red-600 h-screen'>container 1</div>
+    <div>container 2</div>
+    <div>container 3</div>
+    </div>
+  )
+}
+
+export default Grid
---
+++ src/components/flex/flex.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Flex() {
+  return (
+    <div className='border-4 border-sky-400'>
+        <div>
+            <h1>Sova</h1>
+            <p>Ele é o caçador</p>
+            <div>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fview%2Fghost-comms-spray-sova-sage-reyna-jett-gif-26098758&psig=AOvVaw3cklU5OEf5NUe9-ZFnouSO&ust=1723034461228000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCmt_Cw4IcDFQAAAAAdAAAAABAE" alt="Imagem do sova" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Flex

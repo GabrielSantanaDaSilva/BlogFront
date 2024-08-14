e185d0a47ac467cdc0f447b606fc826f8d015134
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-08-14 11:50:40 -0300

    Feat: Crud Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './components/navBar/NavBar';
+import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login/login';
+import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/Home/Home';
+import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 
 
 function App() {
@@ -22,6 +26,11 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
             </Routes>
           </div>
           <Footer />
